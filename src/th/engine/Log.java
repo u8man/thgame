39a521e330700de3adec382237e2952410a335e5
u8man <u8man@manuylenko.ru>
2020-02-17 21:32:33 +0500

    Упрощает класс логирования

--- src/th/engine/Log.java
+++ src/th/engine/Log.java
@@ -1,23 +1,15 @@
 package th.engine;
 
-import th.Game;
-
 /**
  * Лог
  */
 public class Log {
 
-    // Выводит строку в консоль
+    // Выводит сообщение в консоль
     public static void console(String message) {
-        System.out.println(message);
-    }
-
-    // Выводит строку в консоль, если включен режим отладки
-    public static void debug(String message) {
-        if (Game.DEBUG) {
-            console(message);
+        if (Core.isDebug()) {
+            System.out.println(message);
         }
     }
 }
-
 
