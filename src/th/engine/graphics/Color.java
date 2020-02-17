9e89a06b941a5ab35449a35e475a1e518df0cb04
u8man <u8man@manuylenko.ru>
2020-02-18 00:16:55 +0500

    Расширяет функционал

--- src/th/engine/graphics/Color.java
+++ src/th/engine/graphics/Color.java
@@ -8,17 +8,48 @@ public class Color {
     protected float mR, mG, mB, mA;
 
 
-    // Конструктор (3)
+    // Конструктор
+    public Color(Color color) {
+        mR = color.getRed();
+        mG = color.getGreen();
+        mB = color.getBlue();
+        mA = color.getAlpha();
+    }
+
+    // Конструктор (0.0 - 1.0)
     public Color(float r, float g, float b) {
         this(r, g, b, 1.0f);
     }
 
-    // Конструктор (4)
+    // Конструктор (0.0 - 1.0)
     public Color(float r, float g, float b, float a) {
         mR = r;
         mG = g;
         mB = b;
         mA = a;
+    }
+
+    // Конструктор (0 - 255)
+    public Color(int r, int g, int b) {
+        this(r, g, b, 255);
+    }
+
+    // Конструктор (0 - 255)
+    public Color(int r, int g, int b, int a) {
+        mR = r / 255.0f;
+        mG = g / 255.0f;
+        mB = b / 255.0f;
+        mA = a / 255.0f;
+    }
+
+    // Сравнивает цвет
+    public boolean equals(Object other) {
+        if (other instanceof Color) {
+            Color o = (Color) other;
+            return ((o.getRed() == mR) && (o.getGreen() == mG) && (o.getBlue() == mB) && (o.getAlpha() == mA));
+        }
+
+        return false;
     }
 
     // Получает красный
