67df993af27255874fdc20bbacf0e67774f563ed
u8man <u8man@manuylenko.ru>
2020-02-20 00:50:35 +0500

    Добавляет метод для рисования фигур

--- src/th/engine/Graphics.java
+++ src/th/engine/Graphics.java
@@ -2,6 +2,7 @@ package th.engine;
 
 import org.lwjgl.opengl.GL;
 import th.engine.graphics.Color;
+import th.engine.graphics.Shape;
 
 import static org.lwjgl.opengl.GL11.*;
 
@@ -41,6 +42,12 @@ public class Graphics {
     // Устанавливает текущий цвет рисования
     public void setColor(Color color) {
         glColor4f(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha());
+    }
+
+    // Рисует фигуру
+    public void draw(Shape shape, float xPos, float yPos) {
+        shape.setPosition(xPos, yPos);
+        shape.draw();
     }
 }
 
