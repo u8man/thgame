703d035ebf7e9cf2d7d76d43c186631dd6167cd3
u8man <u8man@manuylenko.ru>
2020-02-20 00:56:41 +0500

    Изменяет метод рисования круга

--- src/th/engine/graphics/shapes/Circle.java
+++ src/th/engine/graphics/shapes/Circle.java
@@ -1,11 +1,12 @@
 package th.engine.graphics.shapes;
 
-import org.lwjgl.opengl.GL11;
+import th.engine.Log;
 import th.engine.graphics.Shape;
+
+import static org.lwjgl.opengl.GL11.*;
 
 public class Circle extends Shape {
 
-    private float[] mCenter;
     private float mRadius;
 
     public Circle(float radius) {
@@ -14,12 +15,17 @@ public class Circle extends Shape {
 
     @Override
     public void draw() {
-        GL11.glBegin(GL11.GL_POLYGON);
-        int segments = 12;
-        for (int i = 0; i < segments; i++) {
-            GL11.glVertex2f((float)Math.sin((i / (float) segments) * Math.PI * 2) * mRadius + getPosX(), (float) Math.cos((i / (float) segments) * Math.PI * 2) * mRadius + getPosY());
+        float step = 3.0f;
+
+        glBegin(GL_POLYGON);
+
+        for (float a = 0.0f; a < 360.0; a += step) {
+            float theta = 2.0f * (float) Math.PI * a / 180.0f;
+            glVertex2f(mRadius * (float) Math.cos(theta) + (mXPos + mRadius),
+                    mRadius * (float) Math.sin(theta) + (mYPos + mRadius));
         }
-        GL11.glEnd();
+
+        glEnd();
     }
 }
 
