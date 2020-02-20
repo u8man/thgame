40292e4d166359de574822044a6a07b46c7a71a6
u8man <u8man@manuylenko.ru>
2020-02-20 19:52:06 +0500

    Добавляет отрисовку объекта железной дороги

--- src/th/game/Railway.java
+++ src/th/game/Railway.java
@@ -5,6 +5,8 @@ import th.engine.game.Object;
 import th.engine.game.ObjectManager;
 import th.engine.game.interfaces.Updatable;
 import th.engine.game.interfaces.Renderable;
+import th.engine.graphics.Color;
+import th.engine.graphics.shapes.Rectangle;
 
 /**
  * Железная дорога
@@ -22,7 +24,16 @@ public class Railway extends Object implements Renderable, Updatable {
 
     @Override
     public void render(Graphics g) {
-        //
+        // Шпалы
+        g.setColor(new Color(128, 89, 80));
+        for (int i = 8; i < 640; i += 32) {
+            g.draw(new Rectangle(116, 16), mXPos - 54, mYPos + i);
+        }
+
+        // Рельсы
+        g.setColor(new Color(80, 80, 80));
+        g.draw(new Rectangle(8, 640), mXPos - 44, mYPos);
+        g.draw(new Rectangle(8, 640), mXPos + 44, mYPos);
     }
 }
 
