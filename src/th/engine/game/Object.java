90928bcecb339f758d481ee9a95ade295851d1ca
u8man <u8man@manuylenko.ru>
2020-02-20 19:08:14 +0500

    Изменяет родительский класс игрового объекта

--- src/th/engine/game/Object.java
+++ src/th/engine/game/Object.java
@@ -1,9 +1,56 @@
 package th.engine.game;
 
+import th.engine.game.interfaces.Type;
+
 /**
  * Интерфейс игрового объекта
  */
-public interface Object {
-    //
+abstract public class Object {
+
+    protected Type mType;
+    protected float mXPos;
+    protected float mYPos;
+    protected boolean mRemoved = false;
+
+    public Object(float xPos, float yPos, Type type) {
+        mXPos = xPos;
+        mYPos = yPos;
+        mType = type;
+    }
+
+    // Получает тип игрового объекта
+    public Type getType() {
+        return mType;
+    }
+
+    // Получает положение объекта по X оси
+    public float getXPos() {
+        return mXPos;
+    }
+
+    // Устанавливает положение объекта по X оси
+    public void setXPos(float XPos) {
+        mXPos = XPos;
+    }
+
+    // Получает положение объекта по Y оси
+    public float getYPos() {
+        return mYPos;
+    }
+
+    // Устанавливает положение объекта по Y оси
+    public void setYPos(float YPos) {
+        mYPos = YPos;
+    }
+
+    // Проверяет, помечен ли объект на удаление
+    public boolean isRemoved() {
+        return mRemoved;
+    }
+
+    // Помечает объект на удаление
+    public void remove() {
+        mRemoved = true;
+    }
 }
 
