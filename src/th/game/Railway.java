b027456ed076cc91fccb1d60a6f47d4fb37ef410
u8man <u8man@manuylenko.ru>
2020-02-20 18:30:13 +0500

    Заменяет два класса на один

---
+++ src/th/game/Railway.java
@@ -0,0 +1,27 @@
+package th.game;
+
+import th.engine.Graphics;
+import th.engine.game.Object;
+import th.engine.game.Updatable;
+import th.engine.graphics.Renderable;
+
+/**
+ * Железная дорога
+ */
+public class Railway extends Object implements Renderable, Updatable {
+
+    public Railway(float xPos, float yPos) {
+        //
+    }
+
+    @Override
+    public void render(Graphics g) {
+        //
+    }
+
+    @Override
+    public void update() {
+        //
+    }
+}
+
