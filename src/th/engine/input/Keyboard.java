88b56c1b51947b8a2f245d17237a8aa978959581
u8man <u8man@manuylenko.ru>
2020-02-20 14:08:44 +0500

    Добавляет класс клавиатуры

---
+++ src/th/engine/input/Keyboard.java
@@ -0,0 +1,21 @@
+package th.engine.input;
+
+import static org.lwjgl.glfw.GLFW.*;
+
+/**
+ * Клавиатура
+ */
+public class Keyboard {
+    public static final int KEY_UP = GLFW_KEY_UP;
+    public static final int KEY_DOWN = GLFW_KEY_DOWN;
+    public static final int KEY_LEFT = GLFW_KEY_LEFT;
+    public static final int KEY_RIGHT = GLFW_KEY_RIGHT;
+    public static final int KEY_W = GLFW_KEY_W;
+    public static final int KEY_S = GLFW_KEY_S;
+    public static final int KEY_A = GLFW_KEY_A;
+    public static final int KEY_D = GLFW_KEY_D;
+    public static final int KEY_ESCAPE = GLFW_KEY_ESCAPE;
+    public static final int KEY_ENTER = GLFW_KEY_ENTER;
+    public static final int KEY_SPASE = GLFW_KEY_SPACE;
+}
+
