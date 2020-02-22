90e66d32cd6ed58b3592a38deef95dcd8e8bf07f
u8man <u8man@manuylenko.ru>
2020-02-22 21:03:22 +0500

    Изменяет имя переменной отвечающей за цвет грузового вагона

--- src/th/game/objects/wagons/FreightWagon.java
+++ src/th/game/objects/wagons/FreightWagon.java
@@ -10,7 +10,7 @@ import th.game.objects.Train;
  */
 public class FreightWagon extends Wagon {
 
-    private Color mFreightColor;
+    private Color mColor;
     private int mId;
 
     public FreightWagon(float xPos, float yPos, int id, int color) {
@@ -19,12 +19,12 @@ public class FreightWagon extends Wagon {
         mId = id;
 
         switch (color) {
-            case 1: mFreightColor = new Color(208, 119, 49); break; // оранжевый
-            case 2: mFreightColor = new Color(159, 71, 87); break; // красный
-            case 3: mFreightColor = new Color(37, 114, 178); break; // синий
-            case 4: mFreightColor = new Color(97, 142, 38); break; // зеленый
-            case 5: mFreightColor = new Color(90, 90, 90); break; // серый
-            case 6: mFreightColor = new Color(77, 88, 181); break; // фиолетовый
+            case 1: mColor = new Color(208, 119, 49); break; // оранжевый
+            case 2: mColor = new Color(159, 71, 87); break; // красный
+            case 3: mColor = new Color(37, 114, 178); break; // синий
+            case 4: mColor = new Color(97, 142, 38); break; // зеленый
+            case 5: mColor = new Color(90, 90, 90); break; // серый
+            case 6: mColor = new Color(77, 88, 181); break; // фиолетовый
         }
     }
 
@@ -44,8 +44,8 @@ public class FreightWagon extends Wagon {
             // Платформа
             g.draw(new Rectangle(128, 250), getXPos(), getYPos() + 20);
             // Груз
-            g.setColor(mFreightColor);
-            g.draw(new Rectangle(118, 240), getXPos()+5, getYPos() + 25);
+            g.setColor(mColor);
+            g.draw(new Rectangle(118, 240), getXPos() + 5, getYPos() + 25);
         }
     }
 }
