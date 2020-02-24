586035d59e204dd1d276a6694f730d57c59ac8eb
u8man <u8man@manuylenko.ru>
2020-02-24 16:10:52 +0500

    Добавляет классу круга конструктор с вторым параметром Параметр отвечает за шаг плавности на указанное количество градусов

--- src/th/engine/graphics/shapes/Circle.java
+++ src/th/engine/graphics/shapes/Circle.java
@@ -7,17 +7,26 @@ import static org.lwjgl.opengl.GL11.*;
 public class Circle extends Shape {
 
     protected float mRadius;
-    protected float mStep  = 3.0f;
+    protected float mStep;
 
+    // Конструктор (1)
     public Circle(float radius) {
+        this(radius, 10.0f);
+    }
+
+    // Конструктор (2)
+    public Circle(float radius, float step) {
         mRadius = radius;
+        // Если вдруг шаг меньше 1-го градуса, делаем его 1 градус
+        mStep = step < 1 ? 1 : step;
     }
 
     @Override
+    // Рисует фигуру (круг)
     public void draw() {
         glBegin(GL_TRIANGLE_FAN);
         for (float a = 0.0f; a < 360.0; a += mStep) {
-            float theta = 2.0f * (float) Math.PI * a / 180.0f;
+            float theta = 1.0f * (float) Math.PI * a / 180.0f;
             glVertex2f(mRadius * (float) Math.cos(theta) + (mXPos + mRadius),
                     mRadius * (float) Math.sin(theta) + (mYPos + mRadius));
         }
