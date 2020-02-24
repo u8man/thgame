992ddb2e1274053a88fc76e51577edd379f40b41
u8man <u8man@manuylenko.ru>
2020-02-24 16:00:27 +0500

    Изменяет метод отрисовки для класса круга

--- src/th/engine/graphics/shapes/Circle.java
+++ src/th/engine/graphics/shapes/Circle.java
@@ -7,6 +7,7 @@ import static org.lwjgl.opengl.GL11.*;
 public class Circle extends Shape {
 
     protected float mRadius;
+    protected float mStep  = 3.0f;
 
     public Circle(float radius) {
         mRadius = radius;
@@ -14,9 +15,8 @@ public class Circle extends Shape {
 
     @Override
     public void draw() {
-        float step = 3.0f;
-        glBegin(GL_POLYGON);
-        for (float a = 0.0f; a < 360.0; a += step) {
+        glBegin(GL_TRIANGLE_FAN);
+        for (float a = 0.0f; a < 360.0; a += mStep) {
             float theta = 2.0f * (float) Math.PI * a / 180.0f;
             glVertex2f(mRadius * (float) Math.cos(theta) + (mXPos + mRadius),
                     mRadius * (float) Math.sin(theta) + (mYPos + mRadius));
